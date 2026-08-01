7fba2dfb6bbf790158a1c1d7ea7bf01ff6f154bd
Shivam Bharti <shivamicse5@gmail.com>
2026-08-01 13:05:03 +0530

    Correct QR_Code link

--- backend/src/components/dialog_share_subject.py
+++ backend/src/components/dialog_share_subject.py
@@ -6,7 +6,7 @@ import io
 
 @st.dialog("Share Class Link")
 def share_subject_dialog(subject_name, subject_code):
-    app_domain = "https://intelligent-ai-attendance-fnl5cwown9lpmxsyveum5y.streamlit.app"
+    app_domain = "https://intelligent-ai-attendance-mf3bubuzdantsa4qdxbgi2.streamlit.app/"
     join_url = f"{app_domain}/?join-code={subject_code}"
 
     st.header("Scan to Join")
